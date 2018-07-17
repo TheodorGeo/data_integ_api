916a28fac6da0283ce62cf17ac8fd4a06e8c024e
Theodor Georgopoulos <thodorisgr0@hotmail.com>
2018-07-17 17:58:40 +0300

    Enable cors to the api's routes

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,13 @@ use Illuminate\Http\Request;
 */
 
 //Route to get all boards basic infos via user's token (id,name, shortlink)
-Route::get('/trello/boards','TrelloController@index');
+Route::get('/trello/boards','TrelloController@index')->middleware('cors');
 
 //Route to get a board's id and name via it's shortLink
-Route::get('/trello/boards/id/{shortLink}', 'TrelloController@shortLink');
+Route::get('/trello/boards/id/{shortLink}', 'TrelloController@shortLink')->middleware('cors');
 
 //Route to get a board's infos (cards,lists,checklists etc) via it's id
-Route::get('/trello/board/id/{id}', 'TrelloController@boardById');
+Route::get('/trello/board/id/{id}', 'TrelloController@boardById')->middleware('cors');
 
 //Route to get a board's infos (cards,lists,checklists etc) via it's shortLink
-Route::get('/trello/board/shortlink/{shortLink}', 'TrelloController@boardByShortLink');
+Route::get('/trello/board/shortlink/{shortLink}', 'TrelloController@boardByShortLink')->middleware('cors');
